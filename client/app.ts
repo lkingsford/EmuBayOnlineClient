eda592199667c71151d5032f37a15c00c9103c66
Lachlan Kingsford <lachlan@nerdygentleman.com>
2021-03-16 13:36:00 +0000

    Can step, not perfect
    Needs to click twice to go through "END TURN" automatic moves, and the hiding chooseable is bugged

--- client/app.ts
+++ client/app.ts
@@ -1,15 +1,18 @@
 import { Client } from 'boardgame.io/client';
-import { State } from 'boardgame.io';
-import { SocketIO } from 'boardgame.io/multiplayer'
+import { Ctx, State } from 'boardgame.io';
+import { SocketIO } from 'boardgame.io/multiplayer';
+import { CreateGameReducer } from 'boardgame.io/internal';
 import { EmuBayRailwayCompany, IEmuBayState } from '../game/game';
 import { Board } from './board';
 import { Ui } from './ui';
 
 import * as PIXI from 'pixi.js'
 
-localStorage.debug = '*';
+//localStorage.debug = '*';
 
-class EmuBayRailwayCompanyClient {
+interface IHistoricState { state: State<IEmuBayState>, automatic: boolean, ctx: Ctx };
+
+export class EmuBayRailwayCompanyClient {
     private client: any;
     private rootElement: HTMLElement;
     constructor(rootElement: HTMLElement, mp?: boolean, playerID?: string | null, matchId?: string, numPlayers: number = 4) {
@@ -27,8 +30,7 @@ class EmuBayRailwayCompanyClient {
             req.onreadystatechange = () => {
                 if (req.readyState == 4 && req.status == 200) {
                     let credentials = req.responseText;
-                    if (playerID && playerID != "-1")
-                    {
+                    if (playerID && playerID != "-1") {
                         this.client = Client({
                             game: EmuBayRailwayCompany,
                             multiplayer: SocketIO(),
@@ -58,20 +60,89 @@ class EmuBayRailwayCompanyClient {
 
     public pixiApp = new PIXI.Application({ backgroundColor: 0xEEEEFF, width: 1000, height: 1000 });
 
+    private theUi: Ui | undefined;
+    private mapState: Board | undefined;
+
     public startLoop(resources: { [index: string]: PIXI.LoaderResource }): void {
-        let mapState: Board = new Board(this.pixiApp, resources);
-        let theUi = new Ui()
-        mapState.start();
+        this.mapState = new Board(this.pixiApp, resources);
+        this.theUi = new Ui(this);
+        this.mapState.start();
         // Subscribe in this order, as UI may change things the board needs
         this.client.subscribe((state: State) => {
-            if (state === null) return;
-            theUi.update(state.G as IEmuBayState, state.ctx, this.client, mapState)
-        });
-        this.client.subscribe((state: State) => {
-            if (state === null) return;
-            mapState.drawMap(state.G as IEmuBayState, state.ctx);
+            this.newestState = state;
+            if (this.atCurrent) {
+                this.SkipToCurrent();
+            }
         });
     }
+
+    public SkipToCurrent() {
+        this.atCurrent = true;
+        if (this.newestState === null) return;
+        this!.theUi!.update(this.newestState.G as IEmuBayState, this.newestState.ctx, this.client, this?.mapState!, this.atCurrent, this.visibleTurnId)
+        this!.mapState!.drawMap(this.newestState.G as IEmuBayState, this.newestState.ctx);
+        this!.visibleTurnId = this.client.log.length - 1;
+    }
+
+    public StepForward() {
+        let allStates = this.GetStateHistory();
+        let nextTurn = this.visibleTurnId;
+        do {
+            ++nextTurn;
+        } while ((nextTurn < allStates.length) && allStates[nextTurn].automatic)
+        this.ReviewTurn(allStates, nextTurn);
+    }
+
+    public StepBack() {
+        let allStates = this.GetStateHistory();
+        let nextTurn = this.visibleTurnId;
+        do {
+            --nextTurn;
+        } while ((nextTurn > 0) && allStates[nextTurn].automatic)
+        this.ReviewTurn(allStates, nextTurn);
+    }
+
+    public JumpToStart() {
+        let allStates = this.GetStateHistory();
+        this.ReviewTurn(allStates, 0);
+    }
+
+    private ReviewTurn(stateHistory: IHistoricState[], turnNumber: number) {
+        // This may need caching in the future - because replays whole game each back or forward
+        let currentTurnId = this.client.log.length - 1;
+        this.visibleTurnId = Math.max(0, Math.min(turnNumber, currentTurnId));
+        // The last turn could be automatic - hence, current might not be strictly true when expected
+        this.atCurrent = (this.visibleTurnId == currentTurnId) || stateHistory.slice(this.visibleTurnId + 1).every(i=>i.automatic);
+
+        let {state, ctx} = stateHistory[this.visibleTurnId];
+        // Get the state at this point
+
+        this!.theUi!.update(state.G as IEmuBayState, ctx, this.client, this?.mapState!, this.atCurrent, this.visibleTurnId)
+        this!.mapState!.drawMap(state.G as IEmuBayState, ctx);
+    }
+
+    private GetStateHistory(): IHistoricState[] {
+        const reducer = CreateGameReducer({ game: EmuBayRailwayCompany });
+        const stateSnapshots: IHistoricState[] = [];
+        let state: State<IEmuBayState> = this.client.initialState;
+
+        stateSnapshots.push({state: this.client.initialState, automatic: false, ctx: this.client.initialState.ctx});
+
+        // TODO: Map these types out correctly
+        this.client.log.forEach((i: any) => {
+            const { action, automatic } = i;
+            // ignore automatic log entries - in example code, but not including until known to be necessary
+            state = reducer(state, action);
+            stateSnapshots.push({ state: state, automatic: automatic, ctx: state.ctx });
+        });
+
+        return stateSnapshots;
+    }
+
+    private newestState: State | null = null;
+
+    private atCurrent: boolean = true;
+    private visibleTurnId: number = 0;
 }
 
 const appElement: HTMLElement = document.getElementById('app')!;
