cbb6e9c952dd15a88b074ef1d1f8cbd709915b1f
Lachlan Kingsford <lachlan@nerdygentleman.com>
2022-05-29 04:49:14 +0000

    Make revenue/profit clearer

--- client/board.ts
+++ client/board.ts
@@ -1,23 +1,38 @@
-import * as PIXI from 'pixi.js'
-import * as State from './state'
+import * as PIXI from "pixi.js";
+import * as State from "./state";
 import {
-    IEmuBayState, MAP, ICoordinates, getAllowedBuildSpaces, getTakeResourceSpaces,
-    resourceCubeCost, resourceCubeRevenue, BuildMode, PseudoStage
-} from '../game/game'
+    IEmuBayState,
+    MAP,
+    ICoordinates,
+    getAllowedBuildSpaces,
+    getTakeResourceSpaces,
+    resourceCubeCost,
+    resourceCubeRevenue,
+    BuildMode,
+    PseudoStage,
+} from "../game/game";
 
-import { Ctx } from 'boardgame.io';
+import { Ctx } from "boardgame.io";
 
-enum CubeTexture { EB, TMLC, LW, Narrow, Resource };
+enum CubeTexture {
+    EB,
+    TMLC,
+    LW,
+    Narrow,
+    Resource,
+}
 
 export class Board extends State.State {
-    constructor(app: PIXI.Application,
-        resources: { [index: string]: PIXI.LoaderResource }) {
+    constructor(
+        app: PIXI.Application,
+        resources: { [index: string]: PIXI.LoaderResource }
+    ) {
         super(app, resources);
         let width = app.screen.width;
         let height = app.screen.height;
-        this.container.interactive = true
+        this.container.interactive = true;
         // -40 is so mute button works
-        this.container.hitArea = new PIXI.Rectangle(0, 0, width, height - 40)
+        this.container.hitArea = new PIXI.Rectangle(0, 0, width, height - 40);
 
         this.playfield = new PIXI.Container();
         this.container.addChild(this.playfield);
@@ -48,11 +63,9 @@ export class Board extends State.State {
         this.onResume();
     }
 
-    onResume(): void {
-    }
+    onResume(): void {}
 
-    onKeyDown(): void {
-    }
+    onKeyDown(): void {}
 
     private static TILE_WIDTH = 187;
     private static TILE_HEIGHT = 215;
@@ -63,8 +76,14 @@ export class Board extends State.State {
 
     public resizeToWidth(newWidth: number): void {
         this.view_scale = newWidth / 2000;
-        this.playfield!.scale = new PIXI.Point(this.view_scale, this.view_scale);
-        this.app.renderer.resize(2000 * this.view_scale, 2000 * this.view_scale);
+        this.playfield!.scale = new PIXI.Point(
+            this.view_scale,
+            this.view_scale
+        );
+        this.app.renderer.resize(
+            2000 * this.view_scale,
+            2000 * this.view_scale
+        );
     }
 
     public drawMap(gamestate: IEmuBayState, ctx: Ctx): void {
@@ -72,16 +91,18 @@ export class Board extends State.State {
         this.terrain?.removeChildren();
         MAP.forEach((terrain) => {
             var texture = Board.tileTextures[terrain.textureIndex];
-            terrain.locations.forEach(xy => {
+            terrain.locations.forEach((xy) => {
                 let tileSprite = new PIXI.Sprite(texture);
                 tileSprite.x = Board.TILE_WIDTH * xy.x + Board.OFFSET_X;
-                tileSprite.y = Board.TILE_HEIGHT * xy.y + Board.OFFSET_Y +
+                tileSprite.y =
+                    Board.TILE_HEIGHT * xy.y +
+                    Board.OFFSET_Y +
                     (xy.x % 2 == 0 ? Board.TILE_HEIGHT / 2 : 0);
                 tileSprite.anchor = new PIXI.Point(0.5, 0.5);
                 tileSprite.interactive = true;
                 tileSprite.on("pointertap", () => {
                     if (this.tileClickedOn) {
-                        this.tileClickedOn(xy)
+                        this.tileClickedOn(xy);
                     }
                 });
                 this.terrain!.addChild(tileSprite);
@@ -94,15 +115,24 @@ export class Board extends State.State {
                 let homeSprite = new PIXI.Sprite(texture);
                 let special_offset = 0;
                 // Special, because two companies start in the same place
-                if (i == 2) { special_offset = Board.TILE_WIDTH * -.3 };
-                if (i == 1) { special_offset = Board.TILE_WIDTH * .3 };
-                homeSprite.x = Board.TILE_WIDTH * co.home.x + Board.OFFSET_X + special_offset;
-                homeSprite.y = Board.TILE_HEIGHT * co.home.y + Board.OFFSET_Y +
+                if (i == 2) {
+                    special_offset = Board.TILE_WIDTH * -0.3;
+                }
+                if (i == 1) {
+                    special_offset = Board.TILE_WIDTH * 0.3;
+                }
+                homeSprite.x =
+                    Board.TILE_WIDTH * co.home.x +
+                    Board.OFFSET_X +
+                    special_offset;
+                homeSprite.y =
+                    Board.TILE_HEIGHT * co.home.y +
+                    Board.OFFSET_Y +
                     (co.home.x % 2 == 0 ? Board.TILE_HEIGHT / 2 : 0);
                 homeSprite.anchor = new PIXI.Point(0.5, 0.5);
                 this.terrain!.addChild(homeSprite);
             }
-        })
+        });
 
         // Draw rails and cubes together
         let CubeLocations: { [index: string]: PIXI.Texture[] } = {};
@@ -114,19 +144,21 @@ export class Board extends State.State {
             }
             location.push(Board.cubeTextures[CubeTexture.Resource]);
             CubeLocations[`${xy.x},${xy.y}`] = location;
-        })
+        });
 
         gamestate.track.forEach((xy) => {
             let location = CubeLocations[`${xy.x},${xy.y}`];
             if (!location) {
                 location = [];
             }
-            location.push(Board.cubeTextures[xy.narrow ? CubeTexture.Narrow : xy.owner!]);
+            location.push(
+                Board.cubeTextures[xy.narrow ? CubeTexture.Narrow : xy.owner!]
+            );
             CubeLocations[`${xy.x},${xy.y}`] = location;
-        })
+        });
 
         MAP.forEach((terrain) => {
-            terrain.locations.forEach(xy => {
+            terrain.locations.forEach((xy) => {
                 let cubesToPlace = CubeLocations[`${xy.x},${xy.y}`];
                 if (!cubesToPlace) {
                     return;
@@ -137,81 +169,101 @@ export class Board extends State.State {
                     sprite.anchor = new PIXI.Point(0.5, 0.5);
                     // If more than one, they're circled around centre of point
                     let angle = (i * 2 * Math.PI) / cubesToPlace.length;
-                    let x = Board.TILE_WIDTH * xy.x + Board.OFFSET_X + Math.sin(angle) * this.cube_place_radius;
-                    let y = Board.TILE_HEIGHT * xy.y + Board.OFFSET_Y + (xy.x % 2 == 0 ? Board.TILE_HEIGHT / 2 : 0)
-                        + Math.cos(angle) * this.cube_place_radius;
+                    let x =
+                        Board.TILE_WIDTH * xy.x +
+                        Board.OFFSET_X +
+                        Math.sin(angle) * this.cube_place_radius;
+                    let y =
+                        Board.TILE_HEIGHT * xy.y +
+                        Board.OFFSET_Y +
+                        (xy.x % 2 == 0 ? Board.TILE_HEIGHT / 2 : 0) +
+                        Math.cos(angle) * this.cube_place_radius;
                     sprite.x = x;
                     sprite.y = y;
                     this.terrain!.addChild(sprite);
                 });
             });
-        })
+        });
 
         let labelStyle = new PIXI.TextStyle();
         labelStyle.fill = "#222222";
         labelStyle.fontWeight = "bold";
-        labelStyle.fontSize = 26;
+        labelStyle.fontSize = 32;
 
         MAP.forEach((terrain) => {
             var texture = Board.tileTextures[terrain.textureIndex];
-            terrain.locations.forEach(xy => {
-               if (xy.label) { 
-                let labelSprite = new PIXI.Text(xy.label!)
+            terrain.locations.forEach((xy) => {
+                if (xy.label) {
+                    let labelSprite = new PIXI.Text(xy.label!);
                     labelSprite.x = Board.TILE_WIDTH * xy.x + Board.OFFSET_X;
-                    labelSprite.y = Board.TILE_HEIGHT * xy.y
-                        + (xy.x % 2 == 0 ? Board.TILE_HEIGHT / 2 : 0)
-                        + (0.2 * Board.TILE_HEIGHT);
+                    labelSprite.y =
+                        Board.TILE_HEIGHT * xy.y +
+                        (xy.x % 2 == 0 ? Board.TILE_HEIGHT / 2 : 0) +
+                        0.2 * Board.TILE_HEIGHT;
                     labelSprite.anchor = new PIXI.Point(0.5, 0.5);
                     labelSprite.style = labelStyle;
                     this.terrain!.addChild(labelSprite);
-                this.terrain!.addChild(labelSprite);
-            }
+                    this.terrain!.addChild(labelSprite);
+                }
             });
         });
 
         let stage = gamestate.pseudoStage;
 
         let costStyle = new PIXI.TextStyle();
-        costStyle.fill = "#ff0000";
+        costStyle.fill = "#ff2222";
         costStyle.stroke = "black";
-        costStyle.strokeThickness = 4;
+        costStyle.strokeThickness = 8;
         costStyle.fontWeight = "bold";
-        costStyle.fontSize = 38;
+        costStyle.fontSize = 72;
 
         let revStyle = new PIXI.TextStyle();
-        revStyle.fill = "#00aa00";
+        revStyle.fill = "#22ff22";
         revStyle.stroke = "black";
-        revStyle.strokeThickness = 4;
+        revStyle.strokeThickness = 8;
         revStyle.fontWeight = "bold";
-        revStyle.fontSize = 38;
+        revStyle.fontSize = 72;
 
         if (stage == PseudoStage.buildingTrack) {
             if (gamestate.buildsRemaining! > 0) {
-                let allowedSpaces = getAllowedBuildSpaces(gamestate, this.buildMode!, gamestate.toAct!);
+                let allowedSpaces = getAllowedBuildSpaces(
+                    gamestate,
+                    this.buildMode!,
+                    gamestate.toAct!
+                );
                 allowedSpaces.forEach((xy) => {
                     let sprite = new PIXI.Sprite(Board.canChooseTexture);
                     sprite.anchor = new PIXI.Point(0.5, 0.5);
                     // If more than one, they're circled around centre of point
                     let x = Board.TILE_WIDTH * xy.x + Board.OFFSET_X;
-                    let y = Board.TILE_HEIGHT * xy.y + Board.OFFSET_Y + (xy.x % 2 == 0 ? Board.TILE_HEIGHT / 2 : 0);
+                    let y =
+                        Board.TILE_HEIGHT * xy.y +
+                        Board.OFFSET_Y +
+                        (xy.x % 2 == 0 ? Board.TILE_HEIGHT / 2 : 0);
                     sprite.x = x;
                     sprite.y = y;
                     this.terrain!.addChild(sprite);
 
-                    let priceSprite = new PIXI.Text(`-₤${(xy).cost}`)
-                    priceSprite.x = x - Board.TILE_WIDTH / 5;
-                    priceSprite.y = sprite.y + (Board.TILE_HEIGHT / 2) - (Board.TILE_HEIGHT / 5);
+                    let priceSprite = new PIXI.Text(`- ₤${xy.cost}`);
+                    priceSprite.x = x;
+                    priceSprite.y =
+                        sprite.y +
+                        Board.TILE_HEIGHT / 2 -
+                        Board.TILE_HEIGHT / 4;
                     priceSprite.anchor = new PIXI.Point(0.5, 0.5);
                     priceSprite.style = costStyle;
                     this.terrain!.addChild(priceSprite);
 
-                    let revSprite = new PIXI.Text(`+₤${(xy).rev}`)
-                    revSprite.x = x + Board.TILE_WIDTH / 5;
-                    revSprite.y = sprite.y + (Board.TILE_HEIGHT / 2) - (Board.TILE_HEIGHT / 5);
+                    let revSprite = new PIXI.Text(`+ ₤${xy.rev}`);
+                    revSprite.x = x;
+                    revSprite.y =
+                        sprite.y -
+                        Board.TILE_HEIGHT / 2 +
+                        Board.TILE_HEIGHT / 4;
                     revSprite.anchor = new PIXI.Point(0.5, 0.5);
                     revSprite.style = revStyle;
                     this.terrain!.addChild(revSprite);
-                })
+                });
             }
         }
 
@@ -219,31 +271,39 @@ export class Board extends State.State {
             let cost = resourceCubeCost(gamestate);
             let rev = resourceCubeRevenue(gamestate, gamestate.toAct!);
 
-            let allowedSpaces = getTakeResourceSpaces(gamestate, gamestate.toAct!);
+            let allowedSpaces = getTakeResourceSpaces(
+                gamestate,
+                gamestate.toAct!
+            );
             allowedSpaces.forEach((xy) => {
                 let sprite = new PIXI.Sprite(Board.canChooseTexture);
                 sprite.anchor = new PIXI.Point(0.5, 0.5);
                 // If more than one, they're circled around centre of point
                 let x = Board.TILE_WIDTH * xy.x + Board.OFFSET_X;
-                let y = Board.TILE_HEIGHT * xy.y + Board.OFFSET_Y + (xy.x % 2 == 0 ? Board.TILE_HEIGHT / 2 : 0);
+                let y =
+                    Board.TILE_HEIGHT * xy.y +
+                    Board.OFFSET_Y +
+                    (xy.x % 2 == 0 ? Board.TILE_HEIGHT / 2 : 0);
                 sprite.x = x;
                 sprite.y = y;
                 this.terrain!.addChild(sprite);
 
-                let priceSprite = new PIXI.Text(`-₤${cost}`)
+                let priceSprite = new PIXI.Text(`-₤${cost}`);
                 priceSprite.x = x - Board.TILE_WIDTH / 5;
-                priceSprite.y = sprite.y + (Board.TILE_HEIGHT / 2) - (Board.TILE_HEIGHT / 5);
+                priceSprite.y =
+                    sprite.y + Board.TILE_HEIGHT / 2 - Board.TILE_HEIGHT / 5;
                 priceSprite.anchor = new PIXI.Point(0.5, 0.5);
                 priceSprite.style = costStyle;
                 this.terrain!.addChild(priceSprite);
 
-                let revSprite = new PIXI.Text(`+₤${rev}`)
+                let revSprite = new PIXI.Text(`+₤${rev}`);
                 revSprite.x = x + Board.TILE_WIDTH / 5;
-                revSprite.y = sprite.y + (Board.TILE_HEIGHT / 2) - (Board.TILE_HEIGHT / 5);
+                revSprite.y =
+                    sprite.y + Board.TILE_HEIGHT / 2 - Board.TILE_HEIGHT / 5;
                 revSprite.anchor = new PIXI.Point(0.5, 0.5);
                 revSprite.style = revStyle;
                 this.terrain!.addChild(revSprite);
-            })
+            });
         }
     }
 
@@ -269,7 +329,6 @@ export class Board extends State.State {
     private static CUBE_TILE_HEIGHT = 128;
     private static CUBE_TILE_Y = 512;
 
-
     static getTextures(resources: { [index: string]: PIXI.LoaderResource }) {
         Board.tileTextures = {};
         for (let ix = 0; ix < 3; ix++) {
@@ -277,8 +336,16 @@ export class Board extends State.State {
                 let srcX = ix * Board.TEXTURE_TILE_WIDTH;
                 let srcY = iy * Board.TEXTURE_TILE_HEIGHT;
                 // TODO: Maybe this shouldn't be hardcoded yaknow
-                Board.tileTextures[ix + iy * 3] = new PIXI.Texture(resources["map_tiles"].texture.baseTexture as PIXI.BaseTexture,
-                    new PIXI.Rectangle(srcX, srcY, Board.TEXTURE_TILE_WIDTH, Board.TEXTURE_TILE_HEIGHT));
+                Board.tileTextures[ix + iy * 3] = new PIXI.Texture(
+                    resources["map_tiles"].texture
+                        .baseTexture as PIXI.BaseTexture,
+                    new PIXI.Rectangle(
+                        srcX,
+                        srcY,
+                        Board.TEXTURE_TILE_WIDTH,
+                        Board.TEXTURE_TILE_HEIGHT
+                    )
+                );
             }
         }
 
@@ -286,8 +353,15 @@ export class Board extends State.State {
             let srcX = 3 * Board.TEXTURE_TILE_WIDTH;
             let srcY = 0;
 
-            Board.canChooseTexture = new PIXI.Texture(resources["map_tiles"].texture.baseTexture as PIXI.BaseTexture,
-                new PIXI.Rectangle(srcX, srcY, Board.TEXTURE_TILE_WIDTH, Board.TEXTURE_TILE_HEIGHT));
+            Board.canChooseTexture = new PIXI.Texture(
+                resources["map_tiles"].texture.baseTexture as PIXI.BaseTexture,
+                new PIXI.Rectangle(
+                    srcX,
+                    srcY,
+                    Board.TEXTURE_TILE_WIDTH,
+                    Board.TEXTURE_TILE_HEIGHT
+                )
+            );
         }
 
         Board.companyTextures = {};
@@ -295,16 +369,30 @@ export class Board extends State.State {
             let srcX = ix * Board.COMPANY_TILE_WIDTH;
             let srcY = Board.COMPANY_TILE_Y;
             // TODO: Maybe this shouldn't be hardcoded yaknow
-            Board.companyTextures[ix] = new PIXI.Texture(resources["map_tiles"].texture.baseTexture as PIXI.BaseTexture,
-                new PIXI.Rectangle(srcX, srcY, Board.COMPANY_TILE_WIDTH, Board.COMPANY_TILE_HEIGHT));
+            Board.companyTextures[ix] = new PIXI.Texture(
+                resources["map_tiles"].texture.baseTexture as PIXI.BaseTexture,
+                new PIXI.Rectangle(
+                    srcX,
+                    srcY,
+                    Board.COMPANY_TILE_WIDTH,
+                    Board.COMPANY_TILE_HEIGHT
+                )
+            );
         }
 
         Board.cubeTextures = {};
         for (let ix = 0; ix < 5; ix++) {
             let srcX = ix * Board.CUBE_TILE_WIDTH;
             let srcY = this.CUBE_TILE_Y;
-            Board.cubeTextures[ix] = new PIXI.Texture(resources["map_tiles"].texture.baseTexture as PIXI.BaseTexture,
-                new PIXI.Rectangle(srcX, srcY, Board.CUBE_TILE_WIDTH, Board.CUBE_TILE_HEIGHT));
+            Board.cubeTextures[ix] = new PIXI.Texture(
+                resources["map_tiles"].texture.baseTexture as PIXI.BaseTexture,
+                new PIXI.Rectangle(
+                    srcX,
+                    srcY,
+                    Board.CUBE_TILE_WIDTH,
+                    Board.CUBE_TILE_HEIGHT
+                )
+            );
         }
     }
 }
